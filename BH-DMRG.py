ad6818fa81e228de9e4b706570fa9839a38660e3
Abuenameh Aiyejina <abuenameh@gmail.com>
2014-04-27 04:15:07 -0400

    Commit

--- BH-DMRG.py
+++ BH-DMRG.py
@@ -7,7 +7,11 @@ import itertools
 import concurrent.futures
 import mathematica as m
 import datetime
+import gtk
+import gobject
+import threading
 from progressbar import *
+from speed import *
 
 numthreads = 15
 
@@ -42,7 +46,7 @@ if delta > 0:
     parmsbase['delta'] = delta
     parmsbase['mu'] = 'delta*2*(random() - 0.5)'
 
-def rundmrg(i, N, t):
+def rundmrg(i, t, N):
     filenameprefix = 'files/BH_' + str(i)
     parms = [dict(parmsbase.items() + { 'N_total' : N, 't' : t }.items())]
     input_file = pyalps.writeInputFiles(filenameprefix, parms)
@@ -51,46 +55,46 @@ def rundmrg(i, N, t):
     for s in data[0]:
         if s.props['observable'] == 'Energy':
             E0 = s.y[0]
-    return [N, t, E0]
-
-# Ns = [1,2,3]#range(1, 2*L, 1)
-# ts = [0.1]
-Ns = range(1, 2*L, 1)
-ts = np.linspace(0.01, 0.3, 100).tolist()
-
-futures = []
-
-E0res = np.zeros([len(Ns), len(ts)])
-E0res.fill(np.NaN)
-
-# widgets = [Percentage()]
-pbar = ProgressBar(widgets=[Percentage(), Bar()], maxval=len(Ns)*len(ts)).start()
-
-start = datetime.datetime.now()
-
-with concurrent.futures.ThreadPoolExecutor(max_workers=numthreads) as executor:
-    for i, Nt in enumerate(itertools.product(Ns, ts)):
-        futures.append(executor.submit(rundmrg, i, Nt[0], Nt[1]))
-    for future in concurrent.futures.as_completed(futures):
-        try:
-            res = future.result()
-        except Exception as exc:
-            print exc
-            pass
-        else:
-            E0res[Ns.index(res[0])][ts.index(res[1])] = res[2]
-        pbar.update(pbar.currval+1)
-
-print
-
-end = datetime.datetime.now()
-print end - start
-
-resi = sys.argv[1]
-resfile = '/home/ubuntu/Dropbox/Amazon EC2/Simulation Results/BH-DMRG/res.' + str(resi) + '.txt'
-# resfile = '/Users/Abuenameh/Documents/Simulation Results/BH-DMRG/res.' + str(resi) + '.txt'
-resf = open(resfile, 'w')
-res = 'Lres[{0}]={1};\nsweeps[{0}]={2};\nmaxstates[{0}]={3};\nNmax[{0}]={4};\nNres[{0}]={5};\ntres[{0}]={6};\nE0res[{0}]={7};\nruntime[{0}]=\"{8}\";\n'.format(resi, L, sweeps, maxstates, Nmax, m.mathformat(Ns), m.mathformat(ts), m.mathformat(E0res), end-start)
-resf.write(res)
+    return [t, N, E0]
 
 
+def runmain():
+    Ns = range(1, 2*L, 1)
+    ts = np.linspace(0.01, 0.3, 100).tolist()
+
+    E0res = np.zeros([len(ts), len(Ns)])
+    E0res.fill(np.NaN)
+
+    start = datetime.datetime.now()
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=numthreads) as executor:
+        futures = [executor.submit(rundmrg, i, tN[0], tN[1]) for i, tN in enumerate(itertools.product(ts, Ns))]
+        for future in gprogress(concurrent.futures.as_completed(futures), size=len(futures)):
+            try:
+                res = future.result()
+            except Exception as exc:
+                print exc
+            else:
+                E0res[ts.index(res[0])][Ns.index(res[1])] = res[2]
+
+    end = datetime.datetime.now()
+    print end - start
+
+    resi = sys.argv[1]
+    resfile = '/home/ubuntu/Dropbox/Amazon EC2/Simulation Results/BH-DMRG/res.' + str(resi) + '.txt'
+    # resfile = '/Users/Abuenameh/Documents/Simulation Results/BH-DMRG/res.' + str(resi) + '.txt'
+    resf = open(resfile, 'w')
+    res = 'Lres[{0}]={1};\nsweeps[{0}]={2};\nmaxstates[{0}]={3};\nNmax[{0}]={4};\nNres[{0}]={5};\ntres[{0}]={6};\nE0res[{0}]={7};\nruntime[{0}]=\"{8}\";\n'.format(resi, L, sweeps, maxstates, Nmax, m.mathformat(Ns), m.mathformat(ts), m.mathformat(E0res), end-start)
+    resf.write(res)
+
+    gtk.main_quit()
+
+def startmain():
+    main_thread = threading.Thread(target=runmain);
+    main_thread.start()
+    return False
+
+if __name__ == '__main__':
+    gobject.timeout_add(1000, startmain)
+    gtk.gdk.threads_init()
+    gtk.main()
