a3930dba6bf7725413f8c87e0032aa5d88e05fd1
ubuntu <abuenameh@gmail.com>
2014-04-29 02:41:26 +0000

    Commit

--- BH-DMRG.py
+++ BH-DMRG.py
@@ -10,10 +10,10 @@ import datetime
 import gtk
 import gobject
 import threading
-from progressbar import *
+import os
 from speed import *
 
-numthreads = 10
+numthreads = 1
 
 L = 50
 sweeps = 4
@@ -31,6 +31,7 @@ else:
     lattice = "inhomogeneous chain lattice"
 
 parmsbase = {
+    'TEMP_DIRECTORY' : "/mnt/BH-DMRG",
     'LATTICE' : lattice,
     'MODEL' : "boson Hubbard",
     'CONSERVED_QUANTUMNUMBERS' : 'N',
@@ -42,25 +43,28 @@ parmsbase = {
     'U' : 1
 }
 
+bhdir = '/mnt/BH-DMRG'
+
 if delta > 0:
     parmsbase['delta'] = delta
     parmsbase['mu'] = 'delta*2*(random() - 0.5)'
 
 def rundmrg(i, t, N):
-    filenameprefix = 'files/BH_' + str(i)
+    filenameprefix = 'BH_' + str(i)
     parms = [dict(parmsbase.items() + { 'N_total' : N, 't' : t }.items())]
     input_file = pyalps.writeInputFiles(filenameprefix, parms)
     res = pyalps.runApplication('/opt/alps/bin/dmrg', input_file, writexml=True)
-    data = pyalps.loadEigenstateMeasurements(pyalps.getResultFiles(prefix=filenameprefix))
-    for s in data[0]:
-        if s.props['observable'] == 'Energy':
-            E0 = s.y[0]
-    return [t, N, E0]
+    # data = pyalps.loadEigenstateMeasurements(pyalps.getResultFiles(prefix=filenameprefix))
+    # for s in data[0]:
+    #     if s.props['observable'] == 'Energy':
+    #         E0 = s.y[0]
+    # return [t, N, E0]
+    return [ts[0], Ns[0], 0]
 
 
 def runmain():
     Ns = range(1, 2*L+1, 2*L)
-    ts = np.linspace(0.01, 0.3, 10).tolist()
+    ts = np.linspace(0.01, 0.3, 1).tolist()
 
     E0res = np.zeros([len(ts), len(Ns)])
     E0res.fill(np.NaN)
@@ -69,8 +73,8 @@ def runmain():
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=numthreads) as executor:
         futures = [executor.submit(rundmrg, i, tN[0], tN[1]) for i, tN in enumerate(itertools.product(ts, Ns))]
-        # for future in gprogress(concurrent.futures.as_completed(futures), size=len(futures)):
-        for future in concurrent.futures.as_completed(futures):
+        for future in gprogress(concurrent.futures.as_completed(futures), size=len(futures)):
+        # for future in concurrent.futures.as_completed(futures):
             try:
                 res = future.result()
             except Exception as exc:
@@ -96,6 +100,8 @@ def startmain():
     return False
 
 if __name__ == '__main__':
+    os.chdir(bhdir)
+    [ os.remove(f) for f in os.listdir(".") ]
     gobject.timeout_add(1000, startmain)
     gtk.gdk.threads_init()
     gtk.main()
